e6b722c537026a541bc838f42a7eb1a657078130
DanTheLyon99 <39721655+DanTheLyon99@users.noreply.github.com>
2018-10-09 16:36:35 -0400

    Add files via upload

--- word_checker.h
+++ word_checker.h
@@ -2,9 +2,15 @@
 #include "board_generator.h"
 #include <stdlib.h>
 
+/* Author: Daniel Lyon 
+   Date: 10/09/2018
+   Description: Contains all function protocols
+   for the word_checker.c program
+*/
 
 int word_checker(struct rolled_dice **game_board, char *word);
 int abides_rules(int i, int j, char *word, struct rolled_dice** game_board, int sub_len, int **visited);
+// returns the gameboard character at the specefied index
 char get_letter(int i, int j, struct rolled_dice **game_board);
 
 int test_word_checker(char **boggle, char *word);
