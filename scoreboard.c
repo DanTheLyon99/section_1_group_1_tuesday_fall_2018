3e33bbef3cba01e7589a0f507595cf4abbd417e4
CPDoe <43180402+CPDoe@users.noreply.github.com>
2018-10-28 19:07:04 -0400

    Update scoreboard.c

--- scoreboard.c
+++ scoreboard.c
@@ -10,36 +10,62 @@
                 beginning with head which has the name name_to_find. If such a
                 User is not in the Linked List, it will return NULL. Helper
                 function to increment_score.*/
-User * findUserWithName ( User * head, char * nameToFind ) {
-    if ( head == NULL ) {
-		    return head;
-	  }
-	  User * current = head;
-	  while ( current != NULL ) {
-		    if ( strcmp ( current->name, nameToFind ) == 0 ) {
-			      return current;
-		    }
-		current = ( current )->next;
-	  }
-	  return NULL;
+User * findUserWithName ( User * head, char * nameToFind )
+{
+    User * current = head;
+    
+    if ( head == NULL )
+    {
+        
+        return current;
+    
+    }
+    
+    while ( current != NULL )
+    {
+        
+        if ( strcmp ( current->name, nameToFind ) == 0 )
+        {
+            
+            return current;
+            
+        }
+        
+        current = ( current )->next;
+        
+    }
+    
+    return NULL;
 }
 
 /* Author: Carlo Pagcanlungan
    Date: 10/09/2018
    Description: Frees every element of the linked list.*/
-void freeAll ( User * head ) {
-    if ( head->next == NULL ) {
-		    free( head );
-	  }
-    else {
-		    User * current = head->next;
-		    User * previous = head;
-		    while ( current != NULL ) {
-			      free ( previous );
-			      previous = current;
-			      current = ( current )->next;
-		    }
-	  }
+void freeAll ( User * head )
+{
+    if ( head->next == NULL )
+    {
+        
+        free( head );
+        
+    }
+    else
+    {
+        
+        User * current = head->next;
+        User * previous = head;
+        
+        while ( current != NULL )
+        {
+            
+            free ( previous );
+            previous = current;
+            current = ( current )->next;
+            
+        }
+        
+    }
+    
 }
 
 /* Author: Carlo Pagcanlungan
@@ -47,20 +73,34 @@ void freeAll ( User * head ) {
    Description: Finds and returns a pointer to the User in the linked list
                 beginning with head at the given index. If such a User is not
                 in the Linked List, it will return NULL.*/
-User * getUserAtIndex ( User * head, int index ) {
-    if ( head == NULL ) {
-		    return NULL;
-	  }
-	  User * current = head;
-	  int count = 0;
-	  while ( current != NULL ) {
-		    if ( count == index + 1 ) {
-			      return current;
-		    }
-		    current = ( current )->next;
-		    count++;
-	  }
-	  return NULL;
+User * getUserAtIndex ( User * head, int index )
+{
+    User * current = head;
+    int count = 0;
+    
+    if ( head == NULL )
+    {
+        
+        return current;
+        
+    }
+    
+    while ( current != NULL )
+    {
+        
+        if ( count == index + 1 )
+        {
+            
+            return current;
+            
+        }
+        
+        current = ( current )->next;
+        count++;
+        
+    }
+    
+    return NULL;
 }
 
 /* Author: Carlo Pagcanlungan
@@ -68,54 +108,93 @@ User * getUserAtIndex ( User * head, int index ) {
    Description: Finds and returns the index of the User in the linked list
                 beginning with head which has the name name_to_find. If such a
                 User is not in the Linked List, it will return -1.*/
-int getIndexOfUserWithName ( User * head, char * nameToFind ) {
-	  if ( head == NULL && head->name != nameToFind ) {
-		    return -1;
-	  }
-	  User * current = head;
-	  int count = 0;
-	  while ( current != NULL ) {
-		    if ( strcmp ( current->name, nameToFind ) ) {
-		        return count;
-		    }
-		    current = ( current )->next;
-		    count++;
-	  }
-	  return -1;
+int getIndexOfUserWithName ( User * head, char * nameToFind )
+{
+    User * current = head;
+    int count = 0;
+    
+    if ( head == NULL && head->name != nameToFind )
+    {
+        
+        return -1;
+        
+    }
+    
+    while ( current != NULL )
+    {
+        
+        if ( strcmp ( current->name, nameToFind ) )
+        {
+            
+            return count;
+            
+        }
+        
+        current = ( current )->next;
+        count++;
+        
+    }
+    
+    return -1;
 }
 
  /* Author: Carlo Pagcanlungan
     Date: 10/09/2018
     Description: Finds whether or not an existing user is already in the list.
                  If they are, returns 1. If not, returns 0.*/
-int userIsInList ( User * head, char * nameToFind ) {
-	  User * current = head;
-	  while ( current != NULL ) {
-		    if ( strcmp ( current->name, nameToFind ) == 0 ) {
-			      return 1;
-		    }
-		    current = ( current )->next;
-	  }
-	  return 0;
+int userIsInList ( User * head, char * nameToFind )
+{
+    User * current = head;
+    
+    while ( current != NULL )
+    {
+        
+        if ( strcmp ( current->name, nameToFind ) == 0 )
+        {
+            
+            return 1;
+            
+        }
+        
+        current = ( current )->next;
+        
+    }
+    
+    return 0;
 }
 
 /* Author: Carlo Pagcanlungan
    Date: 10/09/2018
    Description: Finds and returns the length of the linked list.*/
-int getLength ( User * head ) {
-    if ( head == NULL ) {
-		    return 0;
-	  }
-	  User * current = head;
-	  int count = 0;
-	  while (current != NULL) {
-		    count++;
-		    if ( current->next == NULL ) {
-			      return count;
-		    }
-		    current = (current)->next;
-	  }
-	  return 0;
+int getLength ( User * head )
+{
+    User * current = head;
+    int count = 0;
+    
+    if ( head == NULL )
+    {
+        
+        return count;
+        
+    }
+    
+    while (current != NULL)
+    {
+        
+        count++;
+        
+        if ( current->next == NULL )
+        {
+            
+            return count;
+            
+        }
+        
+        current = (current)->next;
+        
+    }
+    
+    return 0;
 }
 
  /* Author: Carlo Pagcanlungan
@@ -124,42 +203,69 @@ int getLength ( User * head ) {
                  returns it. Returns NULL if called with an empty head,
                  although such a case is not used in the main function
                  add_node.*/
-User * getLastNode ( User * head ) {
-    if ( head == NULL ) {
-		    return head;
-	  }
-	  User * current = head;
-	  while ( current != NULL ) {
-		    if ( current->next == NULL ) {
-			      return current;
-		    }
-		    current = (current)->next;
-	  }
-	  return NULL;
+User * getLastNode ( User * head )
+{
+    User * current = head;
+    
+    if ( head == NULL )
+    {
+        
+        return current;
+        
+    }
+    
+    while ( current != NULL )
+    {
+        
+        if ( current->next == NULL )
+        {
+            
+            return current;
+            
+        }
+        
+        current = (current)->next;
+        
+    }
+    
+    return NULL;
 }
 
 /* Author: Carlo Pagcanlungan
    Date: 10/09/2018
    Description: Displays a visual table of the scoreboard.*/
-void printScoreboard ( User * head ) {
+void printScoreboard ( User * head )
+{
     fprintf ( stdout, "\n" );
     fprintf ( stdout, "---- SCORE BOARD ---- \n" );
-	  if ( head->next != NULL ) {
-		    User * current = head->next;
-		    while ( current != NULL ) {
-			      printf ( "\n" );
-			      printf ( "Player name: %s \n", current->name );
-			      printf ( "High score: %d \n", current->max_score );
-			      printf ( "Games played: %d \n", current->total_games );
-			      printf ( "Total score: %d \n", current->total_score );
-			      printf ( "\n" );
-			      printf ( "--------------------- \n" );
-			      if ( current->next == NULL ) {
-				        break;
-			      }
-			      current = ( current )->next;
-		    }
-	  }
+    
+    if ( head->next != NULL ) {
+        
+        User * current = head->next;
+        
+        while ( current != NULL ) {
+            
+            printf ( "\n" );
+            printf ( "Player name: %s \n", current->name );
+            printf ( "High score: %d \n", current->max_score );
+            printf ( "Games played: %d \n", current->total_games );
+            printf ( "Total score: %d \n", current->total_score );
+            printf ( "\n" );
+            printf ( "--------------------- \n" );
+            
+            if ( current->next == NULL )
+            {
+                
+                break;
+                
+            }
+            
+            current = ( current )->next;
+            
+        }
+        
+    }
+    
 }
 
 /* Author: Carlo Pagcanlungan
