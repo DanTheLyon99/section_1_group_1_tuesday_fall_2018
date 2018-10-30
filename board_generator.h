10cd62c2f568e42430a2495d5c7f2f37f24cc327
Daniel Lyon <dlyon@uoguelph.ca>
2018-10-30 00:11:47 -0400

    Add files via upload

--- board_generator.h
+++ board_generator.h
@@ -6,30 +6,46 @@
 #include <time.h>
 #include <stdlib.h>
 
+/* Author: Daniel Lyon 
+   Date: 29/10/2018
+   Description: Contains all function protocols
+   and structs for the board_generator.c program
+*/
 
-// The pre-set dice which keep their values between games.
-typedef struct preset_dice {
+/* The pre-set dice which keep their values between games*/
+typedef struct presetDice 
+{
 	char *configurations;
 	int position;
 } PresetDice;
 
-// The rolled dice which do not keep their values between games.
-typedef struct rolled_dice {
+/*The rolled dice which do not keep their values between games*/
+typedef struct rolledDice 
+{
 	char character;
 	int position;
 } RolledDice;
 
+/* initializes individual potential chars for each dice*/
+void initializePresetDice( struct presetDice* inputArrayOfDice );
 
-void initialize_preset_dice(struct preset_dice* input_array_of_dice);
-void roll_but_not_shuffle_dice(struct rolled_dice* game_dice,
-		struct preset_dice* input_array_of_dice);
+/* Rolls the dice to be used at the start of each game. Puts the rolled dice in
+   gameDice. Does not shuffle the dice. Helper function*/
+void rollButNotShuffleDice( struct rolledDice* gameDice,
+							struct presetDice* inputArrayOfDice );
 
-void shuffle_rolled_dice_positions(struct rolled_dice* game_dice);
-void print_game_board(struct rolled_dice** game_board);
-void roll_dice(struct rolled_dice** game_board,
-		struct preset_dice* input_array_of_dice);
+/* Shuffles an array of rolled dice. Helper function*/
+void shuffleRolledDicePositions( struct rolledDice* gameDice );
 
-void convert_to_board(char *letters, char ***board);
-void print_hc_board(char boggle[][4]);
+/* Prints a visualization of the input array of rolledDice*/
+void printGameBoard( struct rolledDice** gameBoard );
+
+/* Rolls the dice to be used at the start of the game and shuffles them, putting
+   the dice into the 2D array gameDice. Call once at the start of every game*/
+void rollDice( struct rolledDice** gameBoard,
+			   struct presetDice* inputArrayOfDice );
+
+/* Converts board from a string version to 2D array. Used in test mode*/
+void convertToBoard( char *letters, char ***board );
 
 #endif
--- dictionary.h
+++ dictionary.h
@@ -1,3 +1,4 @@
+
 #ifndef DICTIONARY_H
 #define DICTIONARY_H
 
@@ -5,18 +6,34 @@
 #define BIG_HASH_SIZE 20000
 #define SMALL_HASH_SIZE 100
 
-typedef struct d_node {
-	char* key;
-    struct d_node *next;
-}DNode;
+/* Author: Daniel Lyon 
+   Date: 29/10/2018
+   Description: Contains all function protocols, constants
+   and structs for the dictionary.c program
+*/
 
-char * copystr (const char *);
-unsigned hash(const char *s);
+/*This is a dictionary node which stores a key
+  used to lookup and store data in the dictionary*/
+typedef struct dNode 
+{
+    char* key;
+    struct dNode *next;
+} DNode;
 
-DNode * lookup (DNode ** dictionary, int hash_size, const char *key);
+/*makes a copy of the input string*/
+char * copystr( const char * );
 
-DNode * insert (DNode ** dictionary, int hash_size, const char * key);
+/*form hash value for string s
+  this produces a starting value in the dictionary array*/
+unsigned hash( const char *s );
 
-void free_dictionary (DNode ** dictionary, int hash_size);
+/*uses the hashvalue to find if the pointer is the same as the key*/
+DNode * lookup( DNode ** dictionary, int hashSize, const char *key );
+
+/*inserts data into the DNode*/
+DNode * insert( DNode ** dictionary, int hashSize, const char * key );
+
+/*Frees all the data that was stored in the DNode struct*/
+void freeDictionary( DNode ** dictionary, int hashSize );
 
 #endif
