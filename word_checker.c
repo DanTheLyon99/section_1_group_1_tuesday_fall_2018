1e2b77bda88c962412a8590ba00540e1c63b1fa8
Jin Long <jingxuan.long@gmail.com>
2018-10-30 12:02:46 -0400

    Update word_checker.c

--- word_checker.c
+++ word_checker.c
@@ -10,105 +10,135 @@ Description: Returns the particular letter on the gameboard given the location
 */
 char getLetter( int i, int j, struct RolledDice **gameBoard )
 {
+    
     return gameBoard[ i ][ j ].character;
+    
 }
 
 /*
 Author: Satjeevan Riar (1018052)
-Date:
-Description:
+Date: 29th September 2018
+Description: Checks if the rules of the boggle game have been been broken or followed correctly
+and returns 1 or 0 to indicate this
 */
 int abidesRules( int i, int j, char *word, struct RolledDice **gameBoard, int subLength, int **visited )
 {
-    char currentLetter;
+    
     char findLetter;
     int adjacentCell;
     int answer;
     int result;
-    int newX;
-    int newY;
+
     // right, digUpRight, up, digUpLeft, left, digDownLeft, down, digDownRight
-    int allX[] = { 0, -1, -1, -1, 0, 1, 1, 1 };
-    int allY[] = { 1, 1, 0, -1, -1, -1, 0, 1 };
-    currentLetter = word[ subLength ];
-    findLetter = word[subLength + 1];
+    int allX[ ] = { 0, -1, -1, -1, 0, 1, 1, 1 };
+    int allY[ ] = { 1, 1, 0, -1, -1, -1, 0, 1 };
+    findLetter = word[ subLength + 1 ];
     answer = ( subLength == ( strlen( word ) - 1 ) );
     result = 0;
-    if( answer ){
+    if( answer )
+    {
+
         return 1;
+
     }
-    for( adjacentCell = 0; adjacentCell < 8; adjacentCell++ ){
+
+    for( adjacentCell = 0; adjacentCell < 8; adjacentCell++ )
+    {
+        
         int newX = i + allX[ adjacentCell ];
         int newY = j + allY[ adjacentCell ];
+
         if( ( newX >= 0 ) &&
         ( newX < 4 ) &&
         ( newY >= 0 ) &&
         ( newY < 4 ) &&
-        toupper( findLetter ) == getLetter( newX, newY, gameBoard ) && !visited[ newX ][ newY ] ){
+        toupper( findLetter ) == getLetter( newX, newY, gameBoard ) && !visited[ newX ][ newY ] )
+        {
+            
             visited[ newX ][ newY ] = 1;
             ++subLength;
             result = abidesRules( newX, newY, word, gameBoard, subLength, visited );
-            if( result ){
+            if( result )
+            {
+
                 return 1;
+
             }
-            else{
+            else
+            {
                 --subLength;
             }
         }
     }
+
     return 0;
+
 }
 
 /*
 Author: Satjeevan Riar (1018052)
-Date:
-Description:
+Date: 29th September 2018
+Description: Checks for matching word entered by the user and if it is correct
+by using the abidesrules function above
 */
 int wordChecker( struct RolledDice **gameBoard, char *word )
 {
-    int row;
-    int letter;
-    int column;
-    int i;
     int m;
     int n;
     int **visited;
+
     visited = malloc( sizeof( int * ) * 4 );
-    for( int i = 0; i < 4; i++ ){
+    for( int i = 0; i < 4; i++ )
+    {
         visited[ i ] = malloc( sizeof( int ) * 4 );
     }
-    for( m = 0; m < 4; m++ ){
-        for( n = 0; n < 4; n++ ){
 
-            if( m == 0 && n == 0 ){
+    for( m = 0; m < 4; m++ )
+    {
+        for( n = 0; n < 4; n++ )
+        {
+
+            if( m == 0 && n == 0 )
+            {
                 visited[ m ][ n ] = 1;
             }
-            else{
+            else
+            {
                 visited[ m ][ n ] = 0;
             }
         }
     }
-    for ( int row = 0; row < 4; row++ ) {
-        for ( int column = 0; column < 4; column++ ) {
-            if( abidesRules( row, column, word, gameBoard, 0, visited ) ){
-                for( int i = 0; i < 4; i++ ){
+    for ( int row = 0; row < 4; row++ )
+    {
+        for ( int column = 0; column < 4; column++ )
+        {
+
+            if( abidesRules( row, column, word, gameBoard, 0, visited ) )
+            {
+                for( int i = 0; i < 4; i++ )
+                {
                     free( visited[ i ] );
                 }
+
                 return 1;
+
             }
         }
     }
+
     free( visited[ 0 ] );
     free( visited[ 1 ] );
     free( visited[ 2 ] );
     free( visited[ 3 ] );
+
     return 0;
+
 }
 
 /*
 Author: Satjeevan Riar (1018052)
-Date:
-Description:
+Date: 29th September 2018
+Description: Tests that the correct letter is recieved by the program using this function
 */
 char testGetLetter( int i, int j, char **boggle )
 {
@@ -117,8 +147,9 @@ char testGetLetter( int i, int j, char **boggle )
 
 /*
 Author: Satjeevan Riar (1018052)
-Date:
-Description:
+Date: 29th September 2018
+Description: Checks if the rules of the boggle game have been been broken or followed correctly
+and returns 1 or 0 to indicate this
 */
 int testAbideRules( int i, int j, char *word, char **gameBoard, int subLength, int **visited )
 {
@@ -127,46 +158,62 @@ int testAbideRules( int i, int j, char *word, char **gameBoard, int subLength, i
     char findLetter;
     int answer;
     int adjacentCell;
-    int newX;
-    int newY;
     int result;
+
     int allX[] = { 0, -1, -1, -1, 0, 1, 1, 1 };
     int allY[] = { 1, 1, 0, -1, -1, -1, 0, 1 };
     currentLetter = word[ subLength ];
     answer = ( subLength == ( strlen( word ) - 1 ) );
     result = 0;
-    if( answer ){
+    if( answer )
+    {
+
         return 1;
+
     }
+
     if( toupper( currentLetter ) == 'Q' &&
-    toupper( word[ subLength + 2 ] ) == testGetLetter( i, j, gameBoard ) ){
+    toupper( word[ subLength + 2 ] ) == testGetLetter( i, j, gameBoard ) )
+    {
         findLetter = word[ subLength + 3 ];
         subLength = subLength + 2;
         currentLetter = word[ subLength ];
     }
-    else{
+    else
+    {
         findLetter = word[ subLength + 1 ];
     }
-    for( adjacentCell = 0; adjacentCell < 8; adjacentCell++ ){
+
+    for( adjacentCell = 0; adjacentCell < 8; adjacentCell++ )
+    {
         int newX = i + allX[ adjacentCell ];
         int newY = j + allY[ adjacentCell ];
+
         if( ( newX >= 0 ) &&
         ( newX < 4 ) &&
         ( newY >= 0 ) &&
         ( newY < 4 ) &&
-        toupper( findLetter ) == testGetLetter( newX, newY, gameBoard ) && !visited[ newX ][ newY ]){
+
+        toupper( findLetter ) == testGetLetter( newX, newY, gameBoard ) && !visited[ newX ][ newY ])
+        {
             visited[ newX ][ newY ] = 1;
             ++subLength;
             result = testAbideRules( newX, newY, word, gameBoard, subLength, visited );
-            if( result ){
+            if( result )
+            {
+
                 return 1;
+
             }
-            else{
+            else
+            {
                 --subLength;
             }
         }
     }
+
     return 0;
+
 }
 
 /*
