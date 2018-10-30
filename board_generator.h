ea0c73b67d290b9db1471b828363b7e649097b30
Daniel Lyon <dlyon@uoguelph.ca>
2018-10-30 11:53:42 -0400

    Add files via upload

--- board_generator.h
+++ board_generator.h
@@ -32,7 +32,7 @@ void initializePresetDice( struct presetDice* inputArrayOfDice );
 /* Rolls the dice to be used at the start of each game. Puts the rolled dice in
    gameDice. Does not shuffle the dice. Helper function*/
 void rollButNotShuffleDice( struct rolledDice* gameDice,
-							struct presetDice* inputArrayOfDice );
+	                        struct presetDice* inputArrayOfDice );
 
 /* Shuffles an array of rolled dice. Helper function*/
 void shuffleRolledDicePositions( struct rolledDice* gameDice );
@@ -43,7 +43,7 @@ void printGameBoard( struct rolledDice** gameBoard );
 /* Rolls the dice to be used at the start of the game and shuffles them, putting
    the dice into the 2D array gameDice. Call once at the start of every game*/
 void rollDice( struct rolledDice** gameBoard,
-			   struct presetDice* inputArrayOfDice );
+	           struct presetDice* inputArrayOfDice );
 
 /* Converts board from a string version to 2D array. Used in test mode*/
 void convertToBoard( char *letters, char ***board );
